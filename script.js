e86ef67cdc9f527d0c74d29e42d27b86cec75649
Akonatoprogram <153837713+Akonatoprogram@users.noreply.github.com>
2024-06-04 14:38:29 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function validateLogin() {
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+
+  if(username === "admin002" && password === "admin123") {
+    alert("Log in successfully");
+    window.location.assign("dashboard.html");
+    return false;
+  } else {
+    alert("Invalid information");
+    return false;
+  }
+}
